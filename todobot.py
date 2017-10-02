f70a83c03842adc61d90d52598b4d2025b6efc61
alljp <allenjosesmailbox@gmail.com>
2017-10-02 22:59:05 +0530

    Add function get_last_msg that returns a tuple of -   chat_id : identifies the specific chat between the Bot and user   text    : the message

--- todobot.py
+++ todobot.py
@@ -22,3 +22,11 @@ def get_updates():
     url = URL + "getUpdates"
     js = get_json_from_url(url)
     return js
+
+
+def get_last_msg(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    text = updates["result"][last_update]["message"]
+    return (chat_id, text)
