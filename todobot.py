43627bfa9f6b679537720e4023544e1bcf289ece
alljp <allenjosesmailbox@gmail.com>
2017-10-02 21:24:16 +0530

    Basic setup   Define the required globals   Define the base helpers - get_url and get_json_from_url

---
+++ todobot.py
@@ -0,0 +1,18 @@
+import json
+import requests
+import os
+
+TOKEN = os.environ['BOT_API_TOKEN']
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
