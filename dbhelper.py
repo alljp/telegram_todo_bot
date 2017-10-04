2d5d3315e2f0766221bd20434af52c31d3a88295
alljp <allenjosesmailbox@gmail.com>
2017-10-04 12:36:40 +0530

    Add setup function that creates the table

--- dbhelper.py
+++ dbhelper.py
@@ -6,3 +6,8 @@ class DBHelper:
     def __init__(self, dbname="todo.sqlite"):
         self.dbname = dbname
         self.conn = sqlite3.connect(dbname)
+
+    def setup(self):
+        self.conn.execute(
+            "CREATE TABLE IF NOT EXISTS items (description text)")
+        self.conn.commit()
