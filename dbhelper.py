d411daf61499e8dd8e668c4af73a7c18d63b3a10
alljp <allenjosesmailbox@gmail.com>
2017-10-05 16:33:00 +0530

    Modify code to include chat_id for each text, to implement ownership

--- dbhelper.py
+++ dbhelper.py
@@ -9,19 +9,21 @@ class DBHelper:
 
     def setup(self):
         self.conn.execute(
-            "CREATE TABLE IF NOT EXISTS items (description text)")
+            "CREATE TABLE IF NOT EXISTS items (description text, owner text)")
         self.conn.commit()
 
-    def add_item(self, item_text):
+    def add_item(self, item_text, owner):
         self.conn.execute(
-            "INSERT INTO items (description) VALUES (?)", (item_text, ))
+            "INSERT INTO items (description, owner) VALUES (?, ?)",
+            (item_text, owner))
         self.conn.commit()
 
-    def delete_item(self, item_text):
+    def delete_item(self, item_text, owner):
         self.conn.execute(
-            "DELETE FROM items WHERE description = (?)", (item_text, ))
+            "DELETE FROM items WHERE description = (?) AND owner = (?)",
+            (item_text, owner))
         self.conn.commit()
 
-    def get_items(self):
+    def get_items(self, owner):
         return [x[0] for x in self.conn.execute(
-                "SELECT description FROM items")]
+                "SELECT description FROM items WHERE owner=(?)", (owner, ))]
